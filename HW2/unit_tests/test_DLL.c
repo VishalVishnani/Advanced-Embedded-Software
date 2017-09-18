05f1e51f1ff80a1c177d7a51c5375d91adbaf836
Vishal Vishnani <vishalvishnani30@gmail.com>
2017-09-18 17:59:05 -0600

    Edited tests for double linked list

--- HW2/unit_tests/test_DLL.c
+++ HW2/unit_tests/test_DLL.c
@@ -1,3 +1,10 @@
+/**********************************************************
+* Author: Vishal Vishnani
+* Date: 9/6/2017
+* File: test_DLL.c
+* Description: Test file for Double Linked List
+***********************************************************/
+
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -8,18 +15,22 @@
 
 #include "DLL.h"
 
+/*This function tests if adding to an empty list at position 0 is successful*/
 void test_DLL_add_emptylist(void ** state){
   struct Node* head=NULL;
   dll_state status=add_node(&head,0,0);
   assert_int_equal(status,SUCCESS);
 }
 
+/*This function tests if adding to an empty list at position other than 0 fails*/
 void test_DLL_add_empty_position_not0(void ** state){
   struct Node* head=NULL;
   dll_state status=add_node(&head,1,0);
   assert_int_equal(status,FAILURE);
 }
 
+
+/*This function tests if adding to a non-empty list at 0 is successful*/
 void test_DLL_add_position0(void ** state){
   struct Node* head=NULL;
   dll_state status=add_node(&head,0,0);
@@ -28,6 +39,7 @@ void test_DLL_add_position0(void ** state){
 
 }
 
+/*This function tests if adding an element at end position of linkedlist is successful*/
 void test_DLL_add_end(void ** state){
   struct Node* head=NULL;
   dll_state status=add_node(&head,0,0);
@@ -36,6 +48,8 @@ void test_DLL_add_end(void ** state){
   assert_int_equal(status,SUCCESS);
 }
 
+
+/*This function tests if adding an element between start and end position of linked position is successful*/
 void test_DLL_add_bet(void ** state){
   struct Node* head=NULL;
   dll_state status=add_node(&head,0,0);
@@ -45,6 +59,8 @@ void test_DLL_add_bet(void ** state){
   assert_int_equal(status,SUCCESS);
 }
 
+
+/*This function tests if adding an element after the end position of linkedlist fails*/
 void test_DLL_add_after_endpos(void ** state){
   struct Node* head=NULL;
   dll_state status=add_node(&head,0,0);
@@ -55,6 +71,7 @@ void test_DLL_add_after_endpos(void ** state){
 }
 
 
+/*This function tests if removing an element from empty linked list fails*/
 void test_DLL_remove_NULL(void ** state){
    struct Node* head=NULL;
    uint32_t data_removed=0;
@@ -62,6 +79,7 @@ void test_DLL_remove_NULL(void ** state){
    assert_int_equal(status,FAILURE);
 }
 
+/*This function tests if removing node at position0 of non-empty linkedlist is successful*/
 void test_DLL_remove_position0(void ** state){
   struct Node* head=NULL;
   dll_state status=add_node(&head,0,0);
@@ -73,6 +91,7 @@ void test_DLL_remove_position0(void ** state){
   assert_int_equal(status,SUCCESS);
 }
 
+/*This function tests if removing node at end position of linkedlist is successful*/
 void test_DLL_remove_end(void ** state){
   struct Node* head=NULL;
   dll_state status=add_node(&head,0,0);
@@ -84,7 +103,7 @@ void test_DLL_remove_end(void ** state){
   assert_int_equal(status,SUCCESS);
 }
 
-
+/*This function tests if removing node between start and end position of linkedlist is successful*/
 void test_DLL_remove_bet(void ** state){
   struct Node* head=NULL;
   dll_state status=add_node(&head,0,0);
@@ -96,6 +115,7 @@ void test_DLL_remove_bet(void ** state){
   assert_int_equal(status,SUCCESS);
 }
 
+/*This function tests if removing a node after the end position of linked list fails*/
 void test_DLL_remove_after_endpos(void ** state){
   struct Node* head=NULL;
   dll_state status=add_node(&head,0,0);
@@ -108,12 +128,14 @@ void test_DLL_remove_after_endpos(void ** state){
 
 }
 
+/*This function tests if the destroy function returns FAILURE, if an invalid pointer to linked list is passed*/
 void test_DLL_destroy_null(void ** state){
   struct Node* head=NULL;
   dll_state status=destroy(head);
   assert_int_equal(status,FAILURE);
 }
 
+/*This function tests the destroy function*/ 
 void test_DLL_destroy(void ** state){
   struct Node* head=NULL;
   dll_state status=add_node(&head,0,0);
@@ -124,9 +146,9 @@ void test_DLL_destroy(void ** state){
   status=remove_node(&head,&data_removed,1);
   status=destroy(head);
   assert_int_equal(status,SUCCESS);
-
 }
 
+/*This function tests if the size returned by find_size function is correct*/
 void test_find_size(void ** state){
   struct Node* head=NULL;
   add_node(&head,0,0);
@@ -137,7 +159,7 @@ void test_find_size(void ** state){
   assert_int_equal(size,4);
 }
 
-
+/*This function tests the search_node function*/
 void test_search_node(void ** state){
   struct Node* head=NULL;
   add_node(&head,0,0);
